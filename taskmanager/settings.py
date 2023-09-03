ce4399039b2c7a4cfde30bf66c216ffc16526440
abdulmls2 <abdullah.m1230@gmail.com>
2023-09-03 22:04:23 +0200

    admin updates

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -134,21 +134,8 @@ STATIC_URL = '/static/'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'youka9822@gmail.com'
-EMAIL_HOST_PASSWORD = 'whukyokvyrmgswls'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 DEFAULT_AUTO_FIELD='django.db.models.AutoField'
-
-
-# send_mail(
-#     "subject",
-#     "message",
-#     "youka9822@gmail.com",
-#     ["abdulboss14@gmail.com",],
-#     fail_silently=False,
-#     auth_user="youka9822@gmail.com",
-#     auth_password="qazwsx.123",
-#     connection=None,
-#     html_message=None
-#     )
